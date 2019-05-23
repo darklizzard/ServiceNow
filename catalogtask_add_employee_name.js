f310f1c9d37b35bbdc65a33d91bd85ce4c8c1ea6
Travis Carroll <tandmcarroll@gmail.com>
2019-05-23 18:46:04 -0400

    Add existing file

---
+++ catalogtask_add_employee_name.js
@@ -0,0 +1,11 @@
+/**
+ * This adds employee name, target date, and pulls name of catalog task
+ * to Short Description of the task created
+ */
+
+var firstName = current.variables.dit_ob_first_name;
+var lastName = current.variables.dit_ob_last_name;
+var targetDate = current.variables.dit_ob_end_date;
+var subjectName = "Offboarding--";
+var shortDescription = subjectName + activity.name;
+task.short_description = "<" + firstName + " " + lastName + ">" + " " + "<" + targetDate + "> " + shortDescription;
