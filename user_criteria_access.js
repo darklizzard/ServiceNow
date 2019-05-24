0686ff59b7e9de76878b64fd7ebf752b1c851682
Travis Carroll <tandmcarroll@gmail.com>
2019-05-24 17:58:44 -0400

    Update user_criteria_access.js

--- user_criteria_access.js
+++ user_criteria_access.js
@@ -1,7 +1,7 @@
-/**
- * This script is used for user criteria to validate the drop down
- * 
- */
+/********************************************************************************
+ * This script is used for user criteria to validate the drop down for the same *
+ * reason                                                                       *
+ ********************************************************************************/
 var dwsAccess = gs.getUser().getRecord().getValue("u_dws_user_type");
 if (dwsAccess == true)
 {
