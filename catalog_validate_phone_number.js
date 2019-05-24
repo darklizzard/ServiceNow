4b021b72e6981fca3e9bd13534252a633a0f2c0a
Travis Carroll <tandmcarroll@gmail.com>
2019-05-24 18:10:59 -0400

    Create catalog_validate_phone_number.js

---
+++ catalog_validate_phone_number.js
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Catalog Client Script Used for validation This script is used to validate  *
+ * phone number variables. It will issue a warning and then wipe out the data *
+ * entered with message for user to enter a valid number.                     *
+ * @UI_Type [All]                                                             *
+ * @Application [Global]                                                      *
+ * @Type [onChange]                                                           *
+ * @Applies_Catalog_Item [True]                                               *
+ * @Applies_Target_Record [False]                                             *
+ ******************************************************************************/
+ function onChange(control, oldValue, newValue, isLoading) {
+	if (isLoading || newValue == '') {
+		return;
+	}
+
+	// Allows formats of (999) 999-9999, 999-999-9999, and 9999999999
+	var pattern = /^[(]?(\d{3})[)]?[-|\s]?(\d{3})[-|\s]?(\d{4})$/;
+		if(!pattern.test(newValue)){
+			alert('Enter a valid phone number');
+			g_form.setValue('phone_number', '');
+		}
+	}
