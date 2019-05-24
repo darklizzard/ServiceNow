a52f0597d6742743d1d336c92d32e29de1660872
Travis Carroll <tandmcarroll@gmail.com>
2019-05-24 17:35:27 -0400

    Update task_append_title_name.js
    Added more doumentation

--- task_append_title_name.js
+++ task_append_title_name.js
@@ -3,7 +3,6 @@
  * This adds employee name, target date, and pulls name of catalog task
  * to Short Description of the task created
  */
-
 var firstName = current.variables.dit_ob_first_name;
 var lastName = current.variables.dit_ob_last_name;
 var targetDate = current.variables.dit_ob_end_date;
