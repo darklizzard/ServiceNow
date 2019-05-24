5b1b6f3e5a3916471077d9bf44f9f38574f3fe71
Travis Carroll <tandmcarroll@gmail.com>
2019-05-24 17:58:37 -0400

    Update task_append_title_name.js

--- task_append_title_name.js
+++ task_append_title_name.js
@@ -1,8 +1,8 @@
-/**
- * Catalog Task - Advance Task Script
- * This adds employee name, target date, and pulls name of catalog task
- * to Short Description of the task created
- */
+/************************************************************************
+ * Catalog Task - Advance Task Script                                   *
+ * This adds employee name, target date, and pulls name of catalog task *
+ * to Short Description of the task created                             *
+ ************************************************************************/
 var firstName = current.variables.dit_ob_first_name;
 var lastName = current.variables.dit_ob_last_name;
 var targetDate = current.variables.dit_ob_end_date;
