c7ec2c7dcdfed6825b743239c005cf541f7335c7
Travis Carroll <tandmcarroll@gmail.com>
2019-05-24 17:58:34 -0400

    Update request_append_name.js

--- request_append_name.js
+++ request_append_name.js
@@ -1,15 +1,14 @@
-/**
- * Catalog Task Run Script
- * This run script is the first to run in workflow after start
- * It will set the Request ticket: short description with date, employee name
- * It will also set the manager variable from the request_for field.
- */
-
- var reqParent = current.request.getRefRecord();
- var firstName=current.variables.dit_ob_first_name;
- var lastName=current.variables.dit_ob_last_name;
- var updatedManager=current.variables.requested_for.getDisplayValue();
- var targetDate=current.variables.dit_ob_start_date;
+/******************************************************************************
+ * Catalog Task Run Script                                                    *
+ * This run script is the first to run in workflow after start                *
+ * It will set the Request ticket: short description with date, employee name *
+ * It will also set the manager variable from the request_for field.          *
+ ******************************************************************************/
+var reqParent = current.request.getRefRecord();
+var firstName=current.variables.dit_ob_first_name;
+var lastName=current.variables.dit_ob_last_name;
+var updatedManager=current.variables.requested_for.getDisplayValue();
+var targetDate=current.variables.dit_ob_start_date;
 
 // Change short description to reflect name of catalog item(s)
 var shortDescription = " DIT Employee On-Boarding";
