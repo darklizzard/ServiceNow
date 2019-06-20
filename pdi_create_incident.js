fb0b28311618256b429fda539dc13c21d1301c18
Carroll <travis.carroll@nc.gov>
2019-06-20 07:28:52 -0400

    Commit

--- pdi_create_incident.js
+++ pdi_create_incident.js
@@ -12,23 +12,23 @@ gs.addInfoMessage(info);
 var caller = gs.getUserID();
 var parent_table = RP.getParameterValue('sysparm_parent_table');
 if (JSUtil.notNil(parent_table)) {
-	var parent_map = new GlideRecord('request_parent_mapping');
-	parent_map.addQuery('parent_table', parent_table);
-	parent_map.query();
-	if (parent_map.next())
-		var requested_for_field = parent_map.getValue('requested_for_field');
-	var parentGR = new GlideRecord(parent_table);
-	parentGR.addQuery('sys_id', RP.getParameterValue('sysparm_parent_sys_id'));
-	parentGR.query();
-	if (parentGR.next())
-		caller = parentGR.getValue(requested_for_field);
+  var parent_map = new GlideRecord('request_parent_mapping');
+  parent_map.addQuery('parent_table', parent_table);
+  parent_map.query();
+  if (parent_map.next())
+    var requested_for_field = parent_map.getValue('requested_for_field');
+  var parentGR = new GlideRecord(parent_table);
+  parentGR.addQuery('sys_id', RP.getParameterValue('sysparm_parent_sys_id'));
+  parentGR.query();
+  if (parentGR.next())
+    caller = parentGR.getValue(requested_for_field);
 }
 current.contact_type = 'self-service';
 current.caller_id = caller;
 if (producer.comments.length > 80)
-	current.short_description = producer.comments.substring(0, 79);
+  current.short_description = producer.comments.substring(0, 79);
 else
-	current.short_description = producer.comments;
+  current.short_description = producer.comments;
 current.description = producer.comments;
 
 var incRPUtil = new LinkRecordProducerToIncident();
