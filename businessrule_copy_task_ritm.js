6399b22bc95b23c6e263ae232fd10fdd0f116749
Travis Carroll <traviscarroll@traviss-mbp.lan>
2020-08-10 10:12:14 -0400

    Add documentation

--- businessrule_copy_task_ritm.js
+++ businessrule_copy_task_ritm.js
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * @Name: NCDIT Copy Task Comments to RITM
+ * @Name: NCDIT Copy Task Comments to RITM.
  * @Filter_Conditions: Additional comments change
  *******************************************************************************/
 (function executeRule(current, previous /*null when async*/) {
- 	var gr= new GlideRecord("sc_req_item");
- 	gr.get(current.getValue("request_item"));
- 	gr.comments=current.comments.getJournalEntry(1);
- 	gr.update();
- })(current, previous);
+  var gr = new GlideRecord('sc_req_item');
+  gr.get(current.getValue('request_item'));
+  gr.comments = current.comments.getJournalEntry(1);
+  gr.update();
+})(current, previous);
