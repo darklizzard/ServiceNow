9dce3db2ea770825cd7fc596c18f23ba76fc0069
Travis Carroll <traviscarroll@traviss-mbp.lan>
2020-08-19 09:40:02 -0400

    Added comments about g_cart

--- client_script_cart_item_quantity.js
+++ client_script_cart_item_quantity.js
@@ -1,5 +1,10 @@
 /**
  * Potential script. Can tweak to push custom quantity variable to cart quantity.
+ *
+ * There are limitation to the OOB sc_cart_item.quantity field. It is a choice list that caps at 10.
+ * If you raise this, it raises across the platform. You could use g_cart.setQuantity(XX) to set the quantity with an if statement but
+ * again you are limited to the choices associated with the OOB quantity (max 10).
+ *
  */
 var cartId = new sn_sc.CartJS().getCartID();
 var quantity = 0;
