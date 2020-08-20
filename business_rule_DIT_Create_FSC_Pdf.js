2ec09541099fb1560e6b9d5ed1b9d21909a17e90
Travis Carroll <traviscarroll@traviss-mbp.lan>
2020-08-20 08:10:36 -0400

    Updated to include delay

--- business_rule_DIT_Create_FSC_Pdf.js
+++ business_rule_DIT_Create_FSC_Pdf.js
@@ -19,25 +19,30 @@
     tempName = 'Change ACAB';
     sourceTable = 'u_dit_cab_fsc';
     global.GeneralDocForm.generate(current.sys_id, sourceTable, tempTable, tempName, 'ACAB ' + current.u_date_period);
-    gs.eventQueue('send.fsc.pdf', current, gs.getUserID(), gs.getUserName());
+    sendPdfDelayed();
   }
   if (type == 'FSC' && json_data != 'No data found for report') {
     tempName = 'Change FSC';
     sourceTable = 'u_dit_cab_fsc';
     global.GeneralDocForm.generate(current.sys_id, sourceTable, tempTable, tempName, 'CAB FSC ' + current.u_date_period);
-    gs.eventQueue('send.cab.fsc', current, gs.getUserID(), gs.getUserName());
+    sendPdfDelayed();
   }
   if (type == 'FSC - Delta' && json_data != 'No data found for report') {
     tempName = 'Change FSC - Delta';
     sourceTable = 'u_dit_cab_fsc';
     global.GeneralDocForm.generate(current.sys_id, sourceTable, tempTable, tempName, 'CAB FSC - Delta ' + current.u_date_period);
-    // When is delta sent out.don't see this in the notificatons
-    gs.eventQueue('send.cab.fsc', current, gs.getUserID(), gs.getUserName());
+    sendPdfDelayed();
   }
   if (type == 'PSO' && json_data != 'No data found for report') {
     tempName = 'Change PSO';
     sourceTable = 'u_dit_cab_fsc';
     global.GeneralDocForm.generate(current.sys_id, sourceTable, tempTable, tempName, 'Change PSO ' + current.u_date_period);
-    gs.eventQueue('send.cab.pso', current, gs.getUserID(), gs.getUserName());
+    sendPdfDelayed();
+  }
+
+  function sendPdfDelayed() {
+    var gdt = new GlideDateTime();
+    gdt.addSeconds(180); // adds 3 minutes to the date/time as an example
+    gs.eventQueueScheduled('send.fsc.pdf', current, gs.getUserID(), gs.getUserName(), gdt);
   }
 })(current, previous);
