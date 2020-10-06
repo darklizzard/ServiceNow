598078f709946086b9312054b616dca9482004af
Travis Carroll <traviscarroll@traviss-mbp.lan>
2020-10-06 13:53:03 -0400

    Update

---
+++ business_rule_copy_task_comments_ritm.js
@@ -0,0 +1,6 @@
+(function executeRule(current, previous /*null when async*/) {
+  var gr = new GlideRecord('sc_req_item');
+  gr.get(current.getValue('request_item'));
+  gr.comments = current.comments.getJournalEntry(1);
+  gr.update();
+})(current, previous);
